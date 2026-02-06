72a333cf9403196ea54c47c1301b71345ee019fc
mohammad subkhan <51844971+masbekkk@users.noreply.github.com>
2026-02-06 13:51:22 +0700

    update build

--- public/build/assets/app-layout-BtOg6EiL.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-NLUB17Id.js";import{A as s}from"./app-sidebar-layout-B2TGIc1f.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
