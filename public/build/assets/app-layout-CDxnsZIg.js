3766e15b32ce0696cbece0e008085aa388340d0c
mohammad subkhan <51844971+masbekkk@users.noreply.github.com>
2026-02-06 14:13:09 +0700

    upd build

---
+++ public/build/assets/app-layout-CDxnsZIg.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-e330mtId.js";import{A as s}from"./app-sidebar-layout-BKyLy50_.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
