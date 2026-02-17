64e2ab526f55bdfc67f570f62ed56fe61c30c98a
mohammad subkhan <51844971+masbekkk@users.noreply.github.com>
2026-02-17 14:00:51 +0700

    reset node and build

--- public/build/assets/app-layout-W2Snioz2.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-C6QvLIuS.js";import{A as s}from"./app-sidebar-layout-BC2G2iyz.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
