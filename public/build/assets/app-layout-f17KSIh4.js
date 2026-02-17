c65225e010cf1527093ca52e2d6c5ea2f8d96ae6
mohammad subkhan <51844971+masbekkk@users.noreply.github.com>
2026-02-17 14:05:39 +0700

    upd build and node

---
+++ public/build/assets/app-layout-f17KSIh4.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-BdbwucuF.js";import{A as s}from"./app-sidebar-layout-vL3rW7nb.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
