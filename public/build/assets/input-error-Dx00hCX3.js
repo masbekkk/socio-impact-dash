bf2945b71de162ed2a3665e2242959f14f794e78
mohammad subkhan <51844971+masbekkk@users.noreply.github.com>
2026-02-19 11:02:56 +0700

    upd node modules

--- public/build/assets/input-error-Dx00hCX3.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-r4I98vwi.js";import{c as o}from"./button-nCW1XM3I.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
