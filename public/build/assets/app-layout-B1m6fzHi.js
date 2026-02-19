e52f5dc2978701fc3e3475553da9efa4a385a609
mohammad subkhan <51844971+masbekkk@users.noreply.github.com>
2026-02-19 09:55:30 +0700

    upd build

--- public/build/assets/app-layout-B1m6fzHi.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-B7YdYy0P.js";import{A as s}from"./app-sidebar-layout-CdZhCiNT.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
--- public/build/assets/input-error-DymLjhvo.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-B7YdYy0P.js";import{c as o}from"./button-BR4xxjuW.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
