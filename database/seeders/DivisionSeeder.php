91619e6bfa1bd54aa2279267ed03321f99208f12
mohammad subkhan <51844971+masbekkk@users.noreply.github.com>
2026-02-19 09:53:07 +0700

    upd projects

--- database/seeders/DivisionSeeder.php
+++ database/seeders/DivisionSeeder.php
@@ -15,25 +15,84 @@ final class DivisionSeeder extends Seeder
     public function run(): void
     {
         $divisions = [
+            // PT Dampak Sosial Indonesia (Socialimpact.ID)
             [
-                'name' => 'Divisi Teknologi Informasi',
-                'code' => 'IT',
-                'description' => 'Information Technology Division',
+                'name' => 'Impact & Learning (PT Dampak Sosial Indonesia (Socialimpact.ID))',
+                'code' => 'impact_learning',
+                'description' => 'Impact & Learning Division',
             ],
             [
-                'name' => 'Divisi Operasional',
-                'code' => 'OPS',
-                'description' => 'Operations Division',
+                'name' => 'Marketing, Bussiness Development & Implementation (PT Dampak Sosial Indonesia (Socialimpact.ID))',
+                'code' => 'marketing_bdi',
+                'description' => 'Marketing & Business Development Division',
             ],
             [
-                'name' => 'Divisi Keuangan',
-                'code' => 'FIN',
+                'name' => 'Divisi regional Surabaya (PT Dampak Sosial Indonesia (Socialimpact.ID))',
+                'code' => 'reg_surabaya',
+                'description' => 'Regional Surabaya Division',
+            ],
+            [
+                'name' => 'Divisi regional Bali (PT Dampak Sosial Indonesia (Socialimpact.ID))',
+                'code' => 'reg_bali',
+                'description' => 'Regional Bali Division',
+            ],
+            [
+                'name' => 'Divisi creative & social media (PT Dampak Sosial Indonesia (Socialimpact.ID))',
+                'code' => 'creative_social',
+                'description' => 'Creative & Social Media Division',
+            ],
+            [
+                'name' => 'Divisi Human Capital (PT Dampak Sosial Indonesia (Socialimpact.ID))',
+                'code' => 'human_capital',
+                'description' => 'Human Capital Division',
+            ],
+            [
+                'name' => 'Divisi Finance (PT Dampak Sosial Indonesia (Socialimpact.ID))',
+                'code' => 'finance',
                 'description' => 'Finance Division',
             ],
+
+            // PT Bamboo Karya Mandiri
             [
-                'name' => 'Divisi Sumber Daya Manusia',
-                'code' => 'SDM',
-                'description' => 'Human Resources Division',
+                'name' => 'PT Bamboo Karya Mandiri',
+                'code' => 'bamboo_karya_mandiri',
+                'description' => 'PT Bamboo Karya Mandiri Unit',
+            ],
+
+            // Yayasan Biru Hijau Lestari
+            [
+                'name' => 'Divisi Partnership, Bussiness Development & Implementation (Yayasan Biru Hijau Lestari)',
+                'code' => 'partnership_bdi_ybhl',
+                'description' => 'Partnership & BDI Division (YBHL)',
+            ],
+            [
+                'name' => 'Divisi Creative and Social Media (Yayasan Biru Hijau Lestari)',
+                'code' => 'creative_social_ybhl',
+                'description' => 'Creative & Social Media Division (YBHL)',
+            ],
+
+            // Yayasan Dampak Keberlanjutan Indonesia
+            [
+                'name' => 'Divisi Creative and Social Media (Yayasan Dampak Keberlanjutan Indonesia)',
+                'code' => 'creative_social_ydki',
+                'description' => 'Creative & Social Media Division (YDKI)',
+            ],
+            [
+                'name' => 'Yayasan Dampak Keberlanjutan Indonesia',
+                'code' => 'yayasan_dampak_keberlanjutan',
+                'description' => 'Yayasan Dampak Keberlanjutan Indonesia Unit',
+            ],
+
+            // Yayasan Ekosistem Berdaya Lestari Indonesia
+            [
+                'name' => 'Divisi Creative and Social Media (Yayasan Ekosistem Berdaya Lestari Indonesia)',
+                'code' => 'creative_social_yebli',
+                'description' => 'Creative & Social Media Division (YEBLI)',
+            ],
+            [
+                'name' => 'Yayasan Ekosistem Berdaya Lestari Indonesia',
+                'code' => 'yayasan_ekosistem_berdaya',
+                'description' => 'Yayasan Ekosistem Berdaya Lestari Indonesia Unit',
             ],
         ];
 
