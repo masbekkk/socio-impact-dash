69040892821921c3be24f0f7fdb070c3a58dd5f1
mohammad subkhan <51844971+masbekkk@users.noreply.github.com>
2026-02-24 07:58:01 +0700

    upd build

---
+++ public/build/assets/app-layout-C_xWNok2.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-Dvm1eIwM.js";import{A as s}from"./app-sidebar-layout-CtomsolK.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
---
+++ public/build/assets/input-error-D24Kmobb.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-Dvm1eIwM.js";import{c as o}from"./button-CSw2XPtx.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
