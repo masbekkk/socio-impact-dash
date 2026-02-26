2941fd4603a687e1e53295195028fbf658dee406
mohammad subkhan <51844971+masbekkk@users.noreply.github.com>
2026-02-26 20:27:28 +0700

    upd build

--- public/build/assets/app-layout-VJoPympT.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-BzyxwwTF.js";import{A as s}from"./app-sidebar-layout-Dj-Pn2yG.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
