a73f4f6042550c71e621e8969bb547e884c3cd19
mohammad subkhan <51844971+masbekkk@users.noreply.github.com>
2026-02-26 07:56:00 +0700

    upd build

--- public/build/assets/app-layout-C_xWNok2.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-Dvm1eIwM.js";import{A as s}from"./app-sidebar-layout-CtomsolK.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
