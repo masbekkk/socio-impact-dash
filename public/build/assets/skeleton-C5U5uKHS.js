23d184a5036eb7e140c84af2011b456eccb09fd2
mohammad subkhan <51844971+masbekkk@users.noreply.github.com>
2026-02-26 20:28:14 +0700

    upd build

---
+++ public/build/assets/skeleton-C5U5uKHS.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-BUKFnDwc.js";import{c as r}from"./button-B1SNt2xO.js";function m({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:r("bg-primary/10 animate-pulse rounded-md",t),...e})}export{m as S};
