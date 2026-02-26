faf8bf304dd87b06b91502427477c312b2968017
mohammad subkhan <51844971+masbekkk@users.noreply.github.com>
2026-02-26 21:50:18 +0700

    upd build

--- public/build/assets/app-layout-BSnfkpDV.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-BUKFnDwc.js";import{A as s}from"./app-sidebar-layout-BMCoekvN.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
--- public/build/assets/chevrons-right-CbBb6x7x.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./button-B1SNt2xO.js";const o=[["path",{d:"m11 17-5-5 5-5",key:"13zhaf"}],["path",{d:"m18 17-5-5 5-5",key:"h8a8et"}]],h=e("chevrons-left",o);const t=[["path",{d:"m6 17 5-5-5-5",key:"xnjwq"}],["path",{d:"m13 17 5-5-5-5",key:"17xmmf"}]],n=e("chevrons-right",t);export{h as C,n as a};
