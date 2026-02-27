9413836690c123367fe0e523447d92d04f36e0bd
mohammad subkhan <51844971+masbekkk@users.noreply.github.com>
2026-02-27 14:47:23 +0700

    upd build

---
+++ public/build/assets/app-layout-Bg8_T3cw.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-CBk89AjF.js";import{A as s}from"./app-sidebar-layout-CqMEOywD.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
