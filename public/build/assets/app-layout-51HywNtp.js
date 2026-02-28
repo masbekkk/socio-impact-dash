924d52017c6104fe51050af1a7ed9211e94ee7cb
mohammad subkhan <51844971+masbekkk@users.noreply.github.com>
2026-02-28 15:48:26 +0700

    upd build

---
+++ public/build/assets/app-layout-51HywNtp.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-C-zkUOVE.js";import{A as s}from"./app-sidebar-layout-DilYS2cd.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
--- public/build/assets/app-layout-CG2A-uKI.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-EoDHxDb5.js";import{A as s}from"./app-sidebar-layout-B1sPYl7m.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
