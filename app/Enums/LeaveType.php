6626b7f3dd6187064c110d81957a9b2b27f154e4
mohammad subkhan <51844971+masbekkk@users.noreply.github.com>
2026-03-01 00:26:26 +0700

    update leavetype

--- app/Enums/LeaveType.php
+++ app/Enums/LeaveType.php
@@ -10,8 +10,22 @@ enum LeaveType: string
     case Sick = 'sick';
     case Unpaid = 'unpaid';
     case Travel = 'travel';
-    case Berduka = 'berduka'; 
+    case Berduka = 'berduka';
     case Wedding = 'wedding';
     case Birth = 'birth';
     case Important = 'important';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::Annual => 'Cuti Tahunan',
+            self::Sick => 'Sakit',
+            self::Unpaid => 'Cuti di Luar Tanggungan',
+            self::Travel => 'Perjalanan Dinas',
+            self::Berduka => 'Berduka',
+            self::Wedding => 'Menikah',
+            self::Birth => 'Melahirkan',
+            self::Important => 'Urusan Penting',
+        };
+    }
 }
