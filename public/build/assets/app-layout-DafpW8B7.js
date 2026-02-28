5714bb6f35f980bcbf6b79ed456e425f8587b589
mohammad subkhan <51844971+masbekkk@users.noreply.github.com>
2026-02-28 13:57:34 +0700

    upd build

---
+++ public/build/assets/app-layout-DafpW8B7.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-CaeClqVo.js";import{A as s}from"./app-sidebar-layout-DdLWU_B7.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
---
+++ public/build/assets/chevron-down-C8fvklzL.js
@@ -0,0 +1,1 @@
+import{a as o}from"./button-zgn8bn50.js";const n=[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]],c=o("chevron-down",n);export{c as C};
