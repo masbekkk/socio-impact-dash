1a7a883b52e3d5b7e529e5655f5c6126b4d13421
Ian Ale <ianalebom@gmail.com>
2026-08-20 21:34:34 +0800

    buildd

---
+++ public/build/assets/app-layout-Cxqf8vgP.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-IRH_jJyn.js";import{A as s}from"./app-sidebar-layout-DPGf3cRK.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
